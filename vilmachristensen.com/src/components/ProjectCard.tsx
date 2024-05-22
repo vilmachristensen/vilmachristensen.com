94a219457b956647c464d948714afa3ff0c81994
Vilma Christensen <vilmachristensen@MacBook-Pro-som-tillhor-Vilma.local>
2024-05-22 11:27:48 +0200

    Added styled components, component ProjectCard and Types.d.ts

---
+++ vilmachristensen.com/src/components/ProjectCard.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+interface ProjectCardProps {
+    width: number,
+    name: string,
+}
+
+const ProjectCard: React.FC<ProjectCardProps> = ({ }) => {
+
+    return (
+        <div>
+            <p>ProjectCard</p>
+        </div>
+    );
+};
+
+export default ProjectCard;
---
+++ vilmachristensen.com/src/types/Types.d.ts
@@ -0,0 +1,8 @@
+interface Project {
+    name: string;
+    course: string;
+    tools: string;
+    mediaURL: string;
+    gradientColor1: string;
+    gradientColor2: string;
+}
